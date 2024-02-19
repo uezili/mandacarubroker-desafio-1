bafd9e4262dc67b478a452f24dba032240d0e72c
Uezili <weslmaciel@gmail.com>
2024-02-19 18:35:12 -0300

    Resolvendo problema de validação do symbol.

--- src/main/java/com/mandacarubroker/domain/stock/RequestStockDTO.java
+++ src/main/java/com/mandacarubroker/domain/stock/RequestStockDTO.java
@@ -5,11 +5,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 public record RequestStockDTO(
-        @Pattern(regexp = "[A-Za-z]{2}\\d", message = "Symbol must be 3 letters followed by 1 number")
-        String symbol,
-        @NotBlank(message = "Company name cannot be blank")
-        String companyName,
-        @NotNull(message = "Price cannot be null")
-        double price
-) {
+                @Pattern(regexp = "[A-Za-z]{3}\\d", message = "Symbol must be 3 letters followed by 1 number") String symbol,
+                @NotBlank(message = "Company name cannot be blank") String companyName,
+                @NotNull(message = "Price cannot be null") double price) {
 }
